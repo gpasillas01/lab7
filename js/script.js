d26e9df10f08a57d20b17292b49748fb740eb092
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-21 03:01:38 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ function init(){
 var name = document.getElementsByClassName('center')[1];  
 var button1 = document.getElementById('entrybutton');
 var results = document.getElementById('textoutput');
-var input1 = document.getElementById('entryinput'); 
- button1.addEventListener('click', alertMeClicked);
-}   
+var input1 = document.getElementById('entryinput');    
 function alertMeClicked(){
   alert(name.textContent + ":"+ input1.value);
   results.innerHTML = input1.value;  
+}
+ button1.addEventListener('click', alertMeClicked);
 }
 window.addEventListener('load', init);
