103149d6683eb1bac4d5b9a49a7e0ed273e377ba
gpasillas01 <60024567+gpasillas01@users.noreply.github.com>
2020-04-21 03:23:39 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 function init(){
- var button1 = document.getElementById('entrybutton');
- 
- var input1 = document.getElementById('entryinput'); 
- 
- var results = document.getElementById('textoutput');
-  
-  var name = document.getElementsByClassName('center')[1]; 
-function alertFunction(){
-	alert(name.textContent + ': ' + message.value);
-}
+	
+  var input = document.getElementById('entryinput');
+	
+  var output = document.getElementById('textoutput');
+	
+  var button = document.getElementById('entrybutton');
+	
+	button.addEventListener('click', 
+   function(){
+    output.innerHTML = input.value;
+    alert("Griselda Pasillas " + input.value);});}
 
-function outputFunction(){
-	display.innerHTML = message.value;
-}
-
-button.addEventListener('click', alertFunction);
-button.addEventListener('click', outputFunction);
-}
 window.addEventListener('load', init);
+
+
